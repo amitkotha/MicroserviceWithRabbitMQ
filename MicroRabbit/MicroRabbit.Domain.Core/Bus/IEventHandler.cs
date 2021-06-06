0806f7637910cb659166cc92a926acb51ff56525
amitkotha <amitkotha1989@gmail.com>
2021-06-07 00:24:35 +0530

    Added Initial framework

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<T>:IEventHandler where T : Event
+    {
+        Task Handle(T @event);
+    }
+
+    public interface IEventHandler
+    {
+    }
+}
---
+++ MicroRabbit/MicroRabit.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MicroRabit.Infra.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
